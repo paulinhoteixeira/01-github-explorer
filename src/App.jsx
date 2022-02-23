bc24ec8f593ec11092a3f1fc779a1846df04e68a
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2022-02-22 21:43:52 -0300

    utilização do useState para criar um contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './styles/global.scss';
 import {RepositoryList} from './components/RepositoyList'
+import { Counter } from './components/Counter';
 
 export function App(){
-    return < RepositoryList />
+    return (
+        <>
+            < RepositoryList />
+            <Counter />        
+        </>
+
+    )
+    
 }
