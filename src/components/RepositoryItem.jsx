16ca4e9c2e0fb74675ce7d6411e8b8e988b562cb
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2022-02-24 21:35:46 -0300

    utilizando a função map para renderizar os reposittorios da api na tela

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,10 +2,10 @@ export function RepositoryItem(props){
     return(
         <li>
         {/* caso a propriedade esteja vazia, vai assumir a palavra unform */}
-        <strong>{props.repository.name ?? "unform"}</strong> 
+        <strong>{props.repository.name}</strong> 
         <p>{props.repository.description}</p>
 
-        <a href={props.repository.link}>
+        <a href={props.repository.html_url}>
             Acessar repositório
         </a>
     </li>
