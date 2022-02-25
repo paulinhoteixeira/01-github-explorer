cf7a64e6965f5702a669a2c6c3916ef6c26d560f
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2022-02-24 21:04:43 -0300

    estilização do componente RepositoryList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './styles/global.scss';
 import {RepositoryList} from './components/RepositoyList'
-import { Counter } from './components/Counter';
+
 
 export function App(){
     return (
-        <>
             < RepositoryList />
-            <Counter />        
-        </>
-
     )
     
 }
